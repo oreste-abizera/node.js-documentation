d9ad440df7e3de38620ae107ba1fb30a211b8042
oreste-abizera <oresteabizera11@gmail.com>
2022-06-08 09:06:01 +0200

    Documenting API paths

--- src/docs/servers.js
+++ src/docs/servers.js
@@ -1,7 +1,7 @@
 module.exports = {
   servers: [
     {
-      url: "http://localhost:4000/todos", // url
+      url: "http://localhost:4000", // url
       description: "Local server", // name
     },
   ],
