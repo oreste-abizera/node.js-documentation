679d47356fd7480b074c427c28007f7b3e1ad5b0
oreste-abizera <oresteabizera11@gmail.com>
2022-06-08 08:26:33 +0200

    Documenting API general information

--- src/docs/basicInfo.js
+++ src/docs/basicInfo.js
@@ -1,3 +1,13 @@
 module.exports = {
-    
-}
+  openapi: "3.0.3", // present supported openapi version
+  info: {
+    title: "Simple Todos API", // short title.
+    description: "A simple todos API", //  desc.
+    version: "1.0.0", // version number
+    contact: {
+      name: "Oreste Abizera", // your name
+      email: "oresteabizera11@gmail.com", // your email
+      url: "web.com", // your website
+    },
+  },
+};
